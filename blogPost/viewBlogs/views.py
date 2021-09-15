eecc76c9de0a55f63383b529067dbbb7e0a8a828
Giridhar <giridharjha666@gmail.com>
2021-09-16 00:39:30 +0530

    fixed view page and created a modal to view the content

--- blogPost/viewBlogs/views.py
+++ blogPost/viewBlogs/views.py
@@ -4,13 +4,4 @@ from landingPage.models import Create_new
 
 def viewBlogs(request):
 	blogs = Create_new.objects.all()
-	# for blog in blogs:
-	# 	author_name = blog.name
-	# 	author_email = blog.email
-	# 	blog_title = blog.title
-	# 	blog_content = blog.content
-	
-	# context = {
-	# 	aut
-	# }
 	return render(request,'viewBlogs/index.html',{'blogs':blogs})
